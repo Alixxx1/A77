345045e36750b19a25a6e6e5409086d33f87bef9
Alixxx1 <59136354+Alixxx1@users.noreply.github.com>
2019-12-22 20:08:49 +0200

    Create A77.py

---
+++ A77.py
@@ -0,0 +1,9 @@
+input os 
+os.sys("clear")
+
+
+print ("     /   | | |     | |  ")
+primt ("    / /| | | |     | |  ") 
+print ("   / / | | | |     | |  ")
+print ("  / /  | | | |___  | |  ")
+print (" /_/   |_| |_____| |_|  ")
